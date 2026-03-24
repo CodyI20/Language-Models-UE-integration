734c7df59bdc98a17ed4902bb1bf4fb2eedd6cee
Codrin Ilie <codrin.ilie10@gmail.com>
2026-03-24 11:42:07 +0100

    Issue #29
    - Added the RuntimeAudioImporter plugin
    - Created blueprint logic for sending HTTP requests to the localhost server running chatterbox
    - Added functionality for playing the audio output from Chatterbox into Unreal at runtime
    
    NOTE: IT IS VERY VEEEEEEEERY SLOW + It lags the application due to high GPU usage, but at least it works

--- LMIntegration/Source/LMIntegration/Private/UsefulBlueprintNodes.cpp
+++ LMIntegration/Source/LMIntegration/Private/UsefulBlueprintNodes.cpp
@@ -1,11 +1,16 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "LMIntegration/Public/UsefulBlueprintNodes.h"
-
+#include "Kismet/BlueprintFunctionLibrary.h"
 
 void UUsefulBlueprintNodes::ClearOnScreenDebugMessages()
 {
 	if (GEngine == nullptr)
 		return;
 	GEngine->ClearOnScreenDebugMessages();
+}
+
+bool UUsefulBlueprintNodes::SaveArrayToFile(const TArray<uint8> ArrayToSave, const FString& FileName)
+{
+	return FFileHelper::SaveArrayToFile(ArrayToSave, *FileName);
 }
